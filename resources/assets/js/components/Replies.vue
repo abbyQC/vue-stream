7e24dfe1bd66eb144ff92264d689f6d61da4a168
Chang Qiu <qcabby1217@gmail.com>
2020-05-19 14:23:38 -0400

    seperate component responsibility

---
+++ resources/assets/js/components/Replies.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="(reply,index) in items">
+      <reply :data="reply" @deleted="remove(index)"></reply>
+    </div>
+    <new-reply :endpoint="endpoint" @created="add"></new-reply>
+  </div>
+</template>
+<script>
+import Collection from "./Collection";
+import Reply from "./Reply.vue";
+import NewReply from "./NewReply.vue";
+
+export default Collection.extend({
+  components: { Reply, NewReply },
+  data() {
+    return {
+      endpoint: location.pathname + "/replies"
+    };
+  }
+});
+</script>
